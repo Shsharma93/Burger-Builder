f4ebf344b85b1dbfb3bf3518ae8ea9ff9fa2d26c
Shashank Kumar <shsharma93@gmail.com>
2019-04-02 21:18:59 +1000

    toolbar added

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,13 +1,13 @@
-import React from "react";
-import classes from "./BuildControls.module.css";
-import BuildControl from "./BuildControl/BuildControl";
+import React from 'react';
+import classes from './BuildControls.module.css';
+import BuildControl from './BuildControl/BuildControl';
 
 const buildControls = props => {
   const controls = [
-    { label: "Salad", type: "salad" },
-    { label: "Cheese", type: "cheese" },
-    { label: "Bacon", type: "bacon" },
-    { label: "Meat", type: "meat" }
+    { label: 'Salad', type: 'salad' },
+    { label: 'Cheese', type: 'cheese' },
+    { label: 'Bacon', type: 'bacon' },
+    { label: 'Meat', type: 'meat' }
   ];
 
   return (
