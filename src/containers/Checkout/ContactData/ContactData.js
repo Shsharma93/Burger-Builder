3cd4b58fc5b6488e7fba6881046d7f6457374826
Shashank Kumar <shsharma93@gmail.com>
2019-04-24 02:12:47 +1000

    contact form updated

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -2,34 +2,82 @@ import React, { Component } from 'react';
 import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.module.css';
 import axios from '../../../axios-orders';
+import axos from 'axios';
 import Spinner from '../../../components/UI/Spinner/Spinner';
+import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
   state = {
-    name: '',
-    email: '',
-    address: {
-      street: '',
-      postcode: ''
+    orderForm: {
+      name: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Your Name'
+        },
+        value: ''
+      },
+      street: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Street number and name'
+        },
+        value: ''
+      },
+      postCode: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Post Code'
+        },
+        value: ''
+      },
+      country: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Country'
+        },
+        value: ''
+      },
+      email: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'email',
+          placeholder: 'Your email'
+        },
+        value: ''
+      },
+      deliveryMethod: {
+        elementType: 'select',
+        elementConfig: {
+          options: [
+            { value: 'fastest', displayValue: 'Fastest' },
+            { value: 'cheapest', displayValue: 'Cheapest' }
+          ]
+        },
+        value: ''
+      }
     },
     loading: false
+  };
+
+  componentDidMount = () => {
+    axos.get('https://restcountries.eu/rest/v2/all').then(result => {
+      console.log(
+        result.data.map(el => {
+          return el.name;
+        })
+      );
+    });
   };
 
   orderHandler = () => {
     this.setState({ loading: true });
     const order = {
       ingredients: this.props.ingredients,
-      price: this.props.price,
-      customer: {
-        name: 'Shashank',
-        address: {
-          street: 'Edward Street',
-          zipCode: '4000',
-          Country: 'Australia'
-        },
-        email: 'shashank@gmail.com'
-      },
-      deliveryMethod: 'Fastest'
+      price: this.props.price
     };
     axios
       .post('/orders.json', order)
@@ -44,40 +92,25 @@ class ContactData extends Component {
   };
 
   render() {
-    let form = (
-      <form>
-        <input
-          className={classes.Input}
-          type='text'
-          name='name'
-          placeholder='Your Name'
+    let formInputs = Object.keys(this.state.orderForm).map(el => {
+      return (
+        <Input
+          key={el}
+          label={el}
+          elementType={this.state.orderForm[el].elementType}
+          elementConfig={this.state.orderForm[el].elementConfig}
+          value={this.state.orderForm[el].value}
         />
-        <input
-          className={classes.Input}
-          type='email'
-          name='email'
-          placeholder='Your Email'
-        />
-        <input
-          className={classes.Input}
-          type='text'
-          name='street'
-          placeholder='Street Name'
-        />
-        <input
-          className={classes.Input}
-          type='text'
-          name='postcode'
-          placeholder='Your Post Code'
-        />
-      </form>
-    );
+      );
+    });
+
+    let form = <form>{formInputs}</form>;
     if (this.state.loading) {
       form = <Spinner />;
     }
     return (
       <div className={classes.ContactData}>
-        <h4>Ente your contact details</h4>
+        <h4>Enter your contact details</h4>
         {form}
         <Button btnType='Success' clicked={this.orderHandler}>
           ORDER
